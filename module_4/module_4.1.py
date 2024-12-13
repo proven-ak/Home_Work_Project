1c74e673c523137fa7a17494af098fc1e7ed98d5
proven_ak <proven@ya.ru>
2024-12-13 16:05:40 +0300

    mod_4_1 test

--- module_4/module_4.1.py
+++ module_4/module_4.1.py
@@ -1,19 +1,27 @@
 # Домашняя работа по уроку "Модули и пакеты"
 
+
+# Основная функция программы
 def main():
 
     from fake_math import divide as fake_divide
     from true_math import divide as true_divide
 
+    # Используем функцию из fake_math для выполнения деления
     result1 = fake_divide(69, 3)
     result2 = fake_divide(3, 0)
+
+    # Используем функцию из true_math для выполнения деления
     result3 = true_divide(49, 7)
     result4 = true_divide(15, 0)
 
+    # Выводим результаты на консоль
     print(result1)
     print(result2)
     print(result3)
     print(result4)
 
+# Проверяем, запущен ли модуль напрямую
 if __name__ == "__main__":
+    # Если это так, вызываем функцию main
     main()
