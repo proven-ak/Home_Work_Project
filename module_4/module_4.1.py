48924bbf7486097145e0d47eb8e4a6494df9f105
proven_ak <proven@ya.ru>
2024-12-13 14:52:35 +0300

    mod_4_1 add if main module_4_1.py

--- module_4/module_4.1.py
+++ module_4/module_4.1.py
@@ -15,17 +15,20 @@ if __name__ == "__main__":          № Проверка если запущен
     main()                          № Вызываем def если запущен этот модуль
 """
 
+def main():
 
-from fake_math import divide as fake_divide
-from true_math import divide as true_divide
+    from fake_math import divide as fake_divide
+    from true_math import divide as true_divide
 
-result1 = fake_divide(69, 3)
-result2 = fake_divide(3, 0)
-result3 = true_divide(49, 7)
-result4 = true_divide(15, 0)
+    result1 = fake_divide(69, 3)
+    result2 = fake_divide(3, 0)
+    result3 = true_divide(49, 7)
+    result4 = true_divide(15, 0)
 
-print(result1)
-print(result2)
-print(result3)
-print(result4)
+    print(result1)
+    print(result2)
+    print(result3)
+    print(result4)
 
+if __name__ == "__main__":
+    main()
