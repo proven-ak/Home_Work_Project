549dbd8ead7aeb05850e5db4718b31875f723a82
proven_ak <proven@ya.ru>
2024-12-13 13:50:06 +0300

    mod_4_1 initial data

---
+++ module_4/module_4.1.py
@@ -0,0 +1,30 @@
+# Домашняя работа по уроку "Модули и пакеты"
+
+"""
+import module2                      # Импорт пространства имен module2
+import module2 as m2                # Импорт пространства имен module2 как m2
+from module2 inport a, b, say_hi    # Импорт некоторых имен
+from module_2 import *              # Импорт всех имен
+
+def main():
+    a = 5
+    b = 10
+    print("Привет")
+
+if __name__ == "__main__":          № Проверка если запущен этот модуль
+    main()                          № Вызываем def если запущен этот модуль
+"""
+
+
+
+
+result1 = fake_divide(69, 3)
+result2 = fake_divide(3, 0)
+result3 = true_divide(49, 7)
+result4 = true_divide(15, 0)
+
+print(result1)
+print(result2)
+print(result3)
+print(result4)
+
