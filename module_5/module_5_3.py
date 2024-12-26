e5ee14aca3bbaefd7e8f395a8860e8457c821c38
proven_ak <proven@ya.ru>
2024-12-26 20:48:09 +0300

    mod_5_3 add code

---
+++ module_5/module_5_3.py
@@ -0,0 +1,94 @@
+
+class House:
+    # Конструктор класса. Инициализирует имя дома и количество этажей.
+    def __init__(self, name, number_of_floors):
+        self.name = name  # Название дома
+        self.number_of_floors = number_of_floors  # Общее количество этажей в доме
+
+    # Метод для перехода на указанный этаж
+    def go_to(self, new_floor):
+        # Проверяем, существует ли указанный этаж в доме
+        if 1 <= new_floor <= self.number_of_floors:
+            print(new_floor)  # Печатаем номер этажа
+        else:
+            print("Такого этажа не существует")  # Сообщение об ошибке, если этаж вне диапазона
+
+    #
+    def __len__(self):
+        return self.number_of_floors
+
+    #
+    def __str__(self):
+        return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
+
+    #
+    def __eq__(self, other):
+        return self.number_of_floors == other.number_of_floors
+
+    #
+    def __lt__(self, other):
+        return self.number_of_floors < other.number_of_floors
+
+    #
+    def __le__(self, other):
+        return self.number_of_floors <= other.number_of_floors
+
+    #
+    def __gt__(self, other):
+        return self.number_of_floors > other.number_of_floors
+
+    #
+    def __ge__(self, other):
+        return self.number_of_floors >= other.number_of_floors
+
+    #
+    def __ne__(self, other):
+        return self.number_of_floors != other.number_of_floors
+
+    #
+    def __add__(self, value):
+        self.number_of_floors += value
+        return self
+
+    #
+    def __radd__(self, value):
+        self.number_of_floors += value
+        return self
+
+    #
+    def __iadd__(self, value):
+        self.number_of_floors += value
+        return self
+
+        # Создание объектов
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+
+# Вывод строк названия и высотности __str__
+print(h1)
+print(h2)
+
+#__eq__
+print(h1 == h2)
+
+#__add__
+h1 = h1 + 10
+print(h1)
+print(h1 == h2)
+
+#__iadd__
+h1 += 10
+print(h1)
+
+#__radd__
+h2 = 10 + h2
+print(h2)
+
+
+print(h1 > h2) # __gt__
+print(h1 >= h2) # __ge__
+
+print(h1 < h2) # __lt__
+print(h1 <= h2) # __le__
+print(h1 != h2) # __ne__
+
