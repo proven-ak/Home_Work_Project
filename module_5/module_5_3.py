cc6fa9af4aaf631c94508658a69ba0b6e8bb89b0
proven_ak <proven@ya.ru>
2024-12-26 20:55:42 +0300

    mod_5_3 add comments

--- module_5/module_5_3.py
+++ module_5/module_5_3.py
@@ -13,54 +13,44 @@ class House:
         else:
             print("Такого этажа не существует")  # Сообщение об ошибке, если этаж вне диапазона
 
-    #
     def __len__(self):
         return self.number_of_floors
 
-    #
     def __str__(self):
         return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
 
-    #
     def __eq__(self, other):
         return self.number_of_floors == other.number_of_floors
 
-    #
     def __lt__(self, other):
         return self.number_of_floors < other.number_of_floors
 
-    #
     def __le__(self, other):
         return self.number_of_floors <= other.number_of_floors
 
-    #
     def __gt__(self, other):
         return self.number_of_floors > other.number_of_floors
 
-    #
     def __ge__(self, other):
         return self.number_of_floors >= other.number_of_floors
 
-    #
     def __ne__(self, other):
         return self.number_of_floors != other.number_of_floors
 
-    #
     def __add__(self, value):
         self.number_of_floors += value
         return self
 
-    #
     def __radd__(self, value):
         self.number_of_floors += value
         return self
 
-    #
     def __iadd__(self, value):
         self.number_of_floors += value
         return self
 
-        # Создание объектов
+
+# Создание объектов
 h1 = House('ЖК Эльбрус', 10)
 h2 = House('ЖК Акация', 20)
 
@@ -68,27 +58,26 @@ h2 = House('ЖК Акация', 20)
 print(h1)
 print(h2)
 
-#__eq__
-print(h1 == h2)
 
-#__add__
+print(h1 == h2)     #__eq__
+
+
 h1 = h1 + 10
 print(h1)
-print(h1 == h2)
+print(h1 == h2)     #__add__
 
-#__iadd__
-h1 += 10
+
+h1 += 10            #__iadd__
 print(h1)
 
-#__radd__
-h2 = 10 + h2
+
+h2 = 10 + h2        #__radd__
 print(h2)
 
 
-print(h1 > h2) # __gt__
-print(h1 >= h2) # __ge__
+print(h1 > h2)      # __gt__
+print(h1 >= h2)     # __ge__
 
-print(h1 < h2) # __lt__
-print(h1 <= h2) # __le__
-print(h1 != h2) # __ne__
-
+print(h1 < h2)      # __lt__
+print(h1 <= h2)     # __le__
+print(h1 != h2)     # __ne__
