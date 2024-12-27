c17220d498ddd57311a8a32495237dec635191e8
proven_ak <proven@ya.ru>
2024-12-27 18:33:53 +0300

    mod_5_5 add def __add__(self, users)

--- module_5/module_5_5.py
+++ module_5/module_5_5.py
@@ -1,23 +1,37 @@
+
 class User:
     def __init__(self, nickname, password, age):
         """
-        nickname -
-        password -
-        age -
+        nickname - имя пользователя, строка
+        password - в хэшированном виде, число
+        age - возраст, число
         """
         self.nickname = nickname
         self.password = password
         self.age = age
 
+    def __add__(self, users):
+        """
+        Добавляет объект User в список, если его там еще нет.
+        users - список объектов User.
+        """
+        # Проверяем наличие пользователя в списке
+        if self not in users:
+            users.append(self)
+        return users
+
 
 class Video:
+    time_now = 0
+    adult_mode = False
+
     def __init__(self, title, duration, time_now, adult_mode):
-        """
-        title - заголовок, строка
-        duration - продолжительность, секунды
-        time_now - секунда остановки (изначально 0)
-        adult_mode - ограничение по возрасту, bool (False по умолчанию)
-        """
+        
+        # title - заголовок, строка
+        # duration - продолжительность, секунды
+        # time_now - секунда остановки (изначально 0)
+        # adult_mode - ограничение по возрасту, bool (False по умолчанию)
+       
         self.title = title
         self.duration = duration
         self.time_now = time_now
@@ -26,23 +40,46 @@ class Video:
 
 class UrTube:
     def __init__(self, users, videos, current_user):
-        """
-        users - список объектов User
-        videos - список объектов Video
-        current_user - текущий пользователь, User
-        """
+        
+        # users - список объектов User
+        # videos - список объектов Video
+        # current_user - текущий пользователь, User
+        
         self.users = users
         self.videos = videos
         self.current_user = current_user
 
 
-# Код для проверки:
-# Создание объектов
-ur = UrTube()
-v1 = Video('Лучший язык программирования 2024 года', 200)
-v2 = Video('Для чего девушкам парень программист?', 10, adult_mode=True)
+# Код для проверки
+if __name__ == "__main__":
+    # Создание списка пользователей
+    users = []
+    videos = []
 
-"""
+    # Создание объектов User
+    us1 = User(nickname="Ivanov", password=123, age=21)
+    us2 = User(nickname="Petrov", password=1234, age=22)
+    us3 = User(nickname="Sidorov", password=12345, age=23)
+
+    # Добавление пользователей
+    us1 + users
+    us2 + users
+    us3 + users
+
+    # Вывод списка пользователей на консоль
+    print(users)
+
+
+    """
+    v1 = Video('Лучший язык программирования 2024 года', 200, 0, False)
+    v2 = Video('Для чего девушкам парень программист?', 10, 0, adult_mode=True)
+
+    v1 + videos
+    v2 + videos
+
+    ur = UrTube(users, )
+
+
 # Добавление видео
 ur.add(v1, v2)
 
