92d7e304dbb287e0ddfa487b99882f7863609093
proven_ak <proven@ya.ru>
2024-12-28 11:05:20 +0300

    mod_5_5 edit def register

--- module_5/module_5_5.py
+++ module_5/module_5_5.py
@@ -12,10 +12,11 @@ class User:
 
     def add(self, users):
         """
-        Добавляет объект User в список, если его там еще нет.
+        Добавляет объект User в список, если его там еще нет
         users - список объектов User.
         """
-        # Проверяем наличие пользователя в списке
+        # Проверяем наличие пользователя в списке, если отсутствует
+        # добавляем в список
         if self not in users:
             users.append(self)
         return users
@@ -37,14 +38,17 @@ class Video:
         self.time_now = time_now
         self.adult_mode = adult_mode
 
-    def __add__(self, videos):
+    def add(self, videos, *video_list):
         """
         Добавляет объект Video в список, если его там еще нет.
-        videos - список объектов Video.
         """
-        # Проверяем наличие video в списке
-        if self not in videos:
-            videos.append(self)
+        # videos - список объектов Video.
+        # video_list - список добавляемых видео
+
+        for video_init in video_list:
+            if video_init.title not in [video.title for video in self.videos]:
+                self.videos.append(video_init)
+        print("!!!!!!!!videos", videos)
         return videos
 
 
@@ -74,22 +78,28 @@ class UrTube:
 
     def register(self, nickname, password, age):
         """
-        Метод register, который принимает три аргумента: nickname,
-        password, age, и добавляет пользователя в список,
-        если пользователя не существует (с таким же nickname).
-        Если существует, выводит на экран: "Пользователь {nickname} уже существует".
-        После регистрации, вход выполняется автоматически.
+        Метод register добавляет пользователя в список users,
+        если пользователя с таким же nickname еще нет.
+        После успешной регистрации, вход выполняется автоматически
+        (current_user становится зарегистрированным пользователем).
         """
-        # nickname - логин пользователя
-        # password - пароль пользователя
-        # age - возраст пользователя
+        # Проверка наличия пользователя с таким же nickname
+        for user in self.users:
+            if user.nickname == nickname:
+                print(f"Пользователь {nickname} уже существует.")
+                return
+        new_user = User(nickname, password, age)
+        self.users.append(new_user)
+        self.current_user = new_user
+        print(f"Пользователь {nickname} успешно зарегистрирован и вошел в систему.")
+
     def log_out(self, current_user):
         """
         Метод log_out для сброса текущего пользователя на None.
         """
         # current_user - текущий пользователь, User
 
-    def add(self, videos, *video_list):
+    def add(self, *video_list):
         """
         Метод принимает неограниченное кол-во объектов класса Video
         и все добавляет в videos, если с таким же названием видео ещё
@@ -132,73 +142,56 @@ class UrTube:
                 return video_item
 
 
-
-
-
 # Код для проверки
 if __name__ == "__main__":
-    # Создание списка пользователей
-    users = []
-    videos = []
+
+    # Инициализация переменных.
+    users = []          # Список пользователей
+    videos = []         # Список видео
 
     # Создание объектов User
-    us1 = User(nickname="Ivanov", password=123, age=21)
-    us2 = User(nickname="Petrov", password=1234, age=22)
-    us3 = User(nickname="Sidorov", password=12345, age=23)
+    # us1 = User(nickname="Ivanov", password=123, age=21)
+    # us2 = User(nickname="Petrov", password=1234, age=22)
+    # us3 = User(nickname="Sidorov", password=12345, age=23)
 
     # Добавление пользователей в список users
-    users.append(us1)
-    users.append(us2)
-    users.append(us3)
+    # users.append(us1)
+    # users.append(us2)
+    # users.append(us3)
 
     # Вывод списка пользователей на консоль
-    #print("-----users :", users)
+    # print("-----users :", users)
 
     # Создание объектов Video
     v1 = Video('Лучший язык программирования 2024 года', 200, 0, False)
     v2 = Video('Для чего девушкам парень программист?', 10, 0, adult_mode=True)
 
-    # Добавление видео в список videos
-    videos.append(v1)
-    videos.append(v2)
-
-    # Вывод списка видео на консоль
-    #print("-----videos: ", videos)
-
     # Создание объектов UrTube
-    ur = UrTube(users, videos, current_user=us1)
-
-    #print("-----ur: ", ur)
+    ur = UrTube(users, videos, current_user=[])
 
     # Добавление видео
-    ur.add(videos, v1, v2)
-
-    #print("-----ur.add: ", ur)
+    ur.add(v1,v2)
 
     # Проверка поиска
     print(ur.get_videos('лучший'))
     print(ur.get_videos('ПРОГ'))
 
-
     # Проверка на вход пользователя и возрастное ограничение
-    ur.watch_video('Для чего девушкам парень программист?')
-    ur.register('vasya_pupkin', 'lolkekcheburek', 13)
-    ur.watch_video('Для чего девушкам парень программист?')
-    ur.register('urban_pythonist', 'iScX4vIJClb9YQavjAgF', 25)
-    ur.watch_video('Для чего девушкам парень программист?')
+    # ur.watch_video('Для чего девушкам парень программист?')
+    # ur.register('vasya_pupkin', 'lolkekcheburek', 13)
+    # ur.watch_video('Для чего девушкам парень программист?')
+    # ur.register('urban_pythonist', 'iScX4vIJClb9YQavjAgF', 25)
+    # ur.watch_video('Для чего девушкам парень программист?')
 
 """
 
 # Проверка входа в другой аккаунт
-
 ur.register('vasya_pupkin', 'F8098FM8fjm9jmi', 55)
-
 print(ur.current_user)
 
 
 
 # Попытка воспроизведения несуществующего видео
-
 ur.watch_video('Лучший язык программирования 2024 года!')
 """
 
