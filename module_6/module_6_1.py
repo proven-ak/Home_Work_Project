6320eb55581a4b4d379a7e2d73d18049bcf3a2e0
proven_ak <proven@ya.ru>
2024-12-30 12:03:56 +0300

    mod_6_1 test success

--- module_6/module_6_1.py
+++ module_6/module_6_1.py
@@ -1,25 +1,28 @@
+
 class Animal:                   # Класс Животное
 
     def __init__(self, name):
 
-        self.alive = True       # живой
-        self.fed = False        # накормленный
+        self.alive = True       # живой (по умолчанию)
+        self.fed = False        # накормленный (по умолчанию)
         self.name = name        # индивидуальное название каждого животного
 
     def eat(self, food):        # метод Съесть
 
-        # eat(self, food) - метод
-        # food - параметр, принимающий объекты классов растений.
-
-        # self.food = food
-
-        # print('+++++food: ', food.name, food.edible)
-        # print('-----self: ', self.name, self.fed)
+        # self - параметр, принимающий объекты классов животных
+        # food - параметр, принимающий объекты классов растений
+        # food.name - название растения
+        # food.edible - съедобность растения
+        # self.fed - сытость животного
+        # self.alive - живое животное
 
         if food.edible:
+            # если растение съедобное
             print(f'{self.name} съел {food.name}')
             self.fed = True
+
         else:
+            # если растение несъедобное
             print(f'{self.name} не стал есть {food.name}')
             self.alive = False
 
@@ -28,26 +31,11 @@ class Plant:                    # Класс Растение
 
     def __init__(self, name):
 
-        self.edible = False     # съедобность
+        self.edible = False     # съедобность растения (по умолчанию)
         self.name = name        # индивидуальное название каждого растения
 
 
-class Mammal(Animal):         # Класс Млекопитающее
-
-    # food - это параметр, принимающий объекты классов растений.
-    # Метод eat должен работать следующим образом:
-    # Если переданное растение(food) съедобное - выводит
-    # на экран ("<self.name> съел <food.name>",
-    # меняется) атрибут fed на True.
-
-    # Если переданное растение(food) не съедобное - выводит на экран
-    # ("<self.name> не стал есть <food.name>",
-    # меняется) атрибут alive на False.
-
-    # Т.е если животному дать съедобное растение, то
-    # животное насытится, если не съедобное - погибнет.
-
-    # def eat(self, food):
+class Mammal(Animal):           # Класс Млекопитающее
     pass
 
 
@@ -58,45 +46,46 @@ class Predator(Animal):         # Класс Хищник
 class Flower(Plant):            # Класс Цветы
 
     def __init__(self, name):
-
         super().__init__(name)
-        self.edible = False     # съедобность
+        self.edible = False     # съедобность (переопределено)
         self.name = name        # индивидуальное название каждого растения
 
 
 class Fruit(Plant):             # Класс Фрукты
-    def __init__(self, name):
 
+    def __init__(self, name):
         super().__init__(name)
-        self.edible = True  # съедобность
-        self.name = name  # индивидуальное название каждого растения
+        self.edible = True      # съедобность (переопределено)
+        self.name = name        # индивидуальное название каждого растения
 
 
 # Выполняемый код(для проверки):
-a1 = Predator('Волк с Уолл-Стрит')
-a2 = Mammal('Хатико')
-p1 = Flower('Цветик семицветик')
-p2 = Fruit('Заводной апельсин')
+if __name__ == "__main__":
 
-print(a1.name)
-# print(a2.name)
-print(p1.name)
-# print(p2.name)
+    a1 = Predator('Волк с Уолл-Стрит')
+    a2 = Mammal('Хатико')
+    p1 = Flower('Цветик семицветик')
+    p2 = Fruit('Заводной апельсин')
 
-print(a1.alive)
-# print(a2.alive)
-# print(a1.fed)
-print(a2.fed)
+    print(a1.name)
+    # print(a2.name)
+    print(p1.name)
+    # print(p2.name)
 
-a1.eat(p1)
-# a2.eat(p1)
-# a1.eat(p2)
-a2.eat(p2)
+    print(a1.alive)
+    # print(a2.alive)
+    # print(a1.fed)
+    print(a2.fed)
 
-print(a1.alive)
-# print(a2.alive)
-# print(a1.fed)
-print(a2.fed)
+    a1.eat(p1)
+    # a2.eat(p1)
+    # a1.eat(p2)
+    a2.eat(p2)
+
+    print(a1.alive)
+    # print(a2.alive)
+    # print(a1.fed)
+    print(a2.fed)
 
 """
 Вывод на консоль:
