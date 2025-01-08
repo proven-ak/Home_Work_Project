5ac2380b703b114daa964d4207a32d8733c5d5c0
proven_ak <proven@ya.ru>
2025-01-08 17:48:59 +0300

    mod_7_2 Edit test code

--- module_7/module_7_2.py
+++ module_7/module_7_2.py
@@ -19,45 +19,42 @@
 # 'Text for tell.', 'Используйте кодировку utf-8.' - сами строки.
 
 def custom_write(file_name, strings):
+    # Создаём словарь для записи позиций строк
+    strings_positions = {}
+
+    # Открываем файл для записи
     file = open(file_name, 'w', encoding='utf-8')
 
+    # Проходим по каждой строке в списке
     for line_number, string in enumerate(strings):
         # enumerate используется для создания итератора, который возвращает кортежи,
         # содержащие индекс и значение каждого элемента из списка strings
 
         # Получаем текущую позицию в байтах перед записью строки
         byte_position = file.tell()
-        print(line_number, byte_position, string)
 
         # Записываем строку с символом новой строки
         file.write(string + '\n')
 
+        # Добавляем в словарь позицию строки
+        strings_positions[(line_number, byte_position)] = string
+
     file.close()
-    return
+    return strings_positions
 
 
+# Пример выполняемого кода
 info = [
-     'Text for tell.',
-     'Используйте кодировку utf-8.',
-     'Because there are 2 languages!',
-     'Спасибо!'
-     ]
+        'Text for tell.',
+        'Используйте кодировку utf-8.',
+        'Because there are 2 languages!',
+        'Спасибо!'
+        ]
 
-custom_write('test.txt', info)
+result = custom_write('test.txt', info)
 
-# # Пример выполняемого кода:
-# info = [
-#     'Text for tell.',
-#     'Используйте кодировку utf-8.',
-#     'Because there are 2 languages!',
-#     'Спасибо!'
-#     ]
-
-
-# result = custom_write('test.txt', info)
-#
-# for elem in result.items():
-#     print(elem)
+for elem in result.items():
+    print(elem)
 
 
 """
