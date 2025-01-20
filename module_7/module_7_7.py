994b209da59c4d09a513f35656639b47237438ae
proven_ak <proven@ya.ru>
2025-01-20 21:16:34 +0300

    mod_7_7 add def add_everything_up

--- module_7/module_7_7.py
+++ module_7/module_7_7.py
@@ -14,6 +14,16 @@
 # то возвращать строковое представление этих двух данных вместе (в том же порядке).
 # Во всех остальных случаях выполнять стандартные действия.
 
+def add_everything_up(a, b):
+    try:
+        result = a + b
+        # Округляем результат до 6 знаков после запятой, если это число с плавающей точкой
+        if isinstance(result, float):
+            result = round(result, 6)
+        return result
+    except TypeError:
+        return f"{a}{b}"
+
 
 # Пример кода:
 print(add_everything_up(123.456, 'строка'))
