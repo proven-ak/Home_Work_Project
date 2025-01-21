b8bbef1236dde1dc80100618f6106326d515810d
proven_ak <proven@ya.ru>
2025-01-21 20:47:46 +0300

    mod_9_1 add try

--- module_9/module_9_1.py
+++ module_9/module_9_1.py
@@ -20,9 +20,12 @@ def apply_all_func(int_list, *functions):
 
     # Перебираем все функции из *functions
     for func in functions:
-
-        # Добавляем в словарь название функции и результат её работы с int_list
-        results[func.__name__] = func(int_list)
+        try:
+            # Пытаемся выполнить функцию с int_list
+            results[func.__name__] = func(int_list)
+        except Exception as e:
+            # В случае ошибки выводим название функции и текст ошибки
+            results[func.__name__] = f"Ошибка: {str(e)}"
 
     # Возвращаем словарь с результатами
     return results
@@ -43,7 +46,7 @@ print(apply_all_func([6, 20, 15, 9], max, min))
 print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
 
 print(apply_all_func(["6", "20", "15", "9"], max, min))
-print(apply_all_func(["6", "20", "15", "9"], len, sum, sorted))
+print(apply_all_func([6, 20, 15, 9], len, sum, setattr))
 
 # Вывод на консоль:
 # {'max': 20, 'min': 6} {'len': 4, 'sum': 50, 'sorted': [6, 9, 15, 20]}
