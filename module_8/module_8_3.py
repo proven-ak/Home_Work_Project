bdf9c4d7523ee9af11b9f775879bb53ac9bb4c71
proven_ak <proven@ya.ru>
2025-01-21 12:22:41 +0300

    mod_8_3 edit class IncorrectCarNumbers

--- module_8/module_8_3.py
+++ module_8/module_8_3.py
@@ -26,7 +26,9 @@ class IncorrectVinNumber(Exception):
 
 
 class IncorrectCarNumbers(Exception):
-    pass
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
 
 
 class Car:
