4d073aadd13bfe64304985b7592909501a1d83da
proven_ak <proven@ya.ru>
2025-01-21 22:56:46 +0300

    mod_9_2 add second_result

--- module_9/module_9_2.py
+++ module_9/module_9_2.py
@@ -12,13 +12,13 @@ second_strings = ['Task', 'Git', 'Comprehension', 'Java', 'Computer', 'Assembler
 # В переменную first_result запишите список созданный при помощи сборки состоящий
 # из длин строк списка first_strings, при условии, что длина строк не менее 5 символов.
 
-first_result = [len(_str) for _str in first_strings if len(_str) >= 5]
+first_result = [len(first_str) for first_str in first_strings if len(first_str) >= 5]
 
 # В переменную second_result запишите список созданный при помощи сборки состоящий
 # из пар слов(кортежей) одинаковой длины. Каждое слово из списка first_strings должно
 # сравниваться с каждым из second_strings. (два цикла)
 
-# second_result = {}
+second_result = [(first_str, second_str) for first_str in first_strings for second_str in second_strings if len(first_str) == len(second_str)]
 
 # В переменную third_result запишите словарь созданный при помощи сборки,
 # где парой ключ-значение будет строка-длина строки. Значения строк будут перебираться
@@ -27,7 +27,7 @@ first_result = [len(_str) for _str in first_strings if len(_str) >= 5]
 
 # Пример выполнения кода:
 print(first_result)
-# print(second_result)
+print(second_result)
 # print(third_result)
 
 # Вывод на консоль:
@@ -37,3 +37,4 @@ print(first_result)
 
 # Примечания:
 # Помните, когда вы используете 2 цикла for внутри сборки, первый цикл - внешний, второй - внутренний.
+
