21c686d40d2955808cd6ec233a09fac7cfec0b91
proven_ak <proven@ya.ru>
2025-01-23 11:10:04 +0300

    mod_9_5 edit code

--- module_9/module_9_5.py
+++ module_9/module_9_5.py
@@ -1,26 +1,17 @@
 
-# Домашнее задание по теме "Итераторы"
+# Задание по теме "Итераторы"
 
-# Цель: освоить механизмы работы итераторов и описания методов __next__ и __iter__.
-# Закрепить навык создания и выбрасывания исключений.
-
-# Задача "Range - это просто":
-
-# Создайте пользовательский класс исключения StepValueError, который наследуется от ValueError.
-# Наследования достаточно, класс оставьте пустым при помощи оператора pass.
-
+# Создание пользовательского исключения
 class StepValueError(Exception):
     pass
 
-# Создайте класс Iterator, который обладает следующими свойствами:
 
 class Iterator():
 
-# Атрибуты объекта:
-# start - целое число, с которого начинается итерация.
-# stop - целое число, на котором заканчивается итерация.
-# step - шаг, с которым совершается итерация.
-# pointer - указывает на текущее число в итерации (изначально start)
+    # start - целое число, с которого начинается итерация.
+    # stop - целое число, на котором заканчивается итерация.
+    # step - шаг, с которым совершается итерация.
+    # pointer - указывает на текущее число в итерации (изначально start)
 
     def __init__(self, start, stop, step=1):
         self.start = start
@@ -28,6 +19,7 @@ class Iterator():
         self.step = step
         self.pointer = start
 
+        # Проверка на нулевой шаг
         if self.step == 0:
             raise StepValueError('шаг не может быть равен 0')
 
@@ -45,27 +37,13 @@ class Iterator():
             self.pointer += self.step
             return current
 
+        # Если шаг отрицательный, проверяем, не пересекли ли мы stop
         elif self.step < 0:
             if self.pointer <= self.stop:
                 raise StopIteration
             current = self.pointer
             self.pointer += self.step
             return current
-
-# Методы:
-# __init__(self, start, stop, step=1) - принимающий значения старта и конца итерации, а также шага.
-# В этом методе в первую очередь проверяется step на равенство 0. Если равно, то выбрасывается
-# исключение StepValueError('шаг не может быть равен 0')
-# __iter__ - метод, сбрасывающий значение pointer на start и возвращающий сам объект итератора.
-# __next__ - метод, увеличивающий атрибут pointer на step. В зависимости от знака атрибута step
-# итерация завершится либо когда pointer станет больше stop, либо меньше stop.
-# Учтите это при описании метода.
-
-
-# Пункты задачи:
-# Создайте класс исключения StepValueError.
-# Создайте класс Iterator и опишите его атрибуты и методы.
-# Создайте несколько объектов класса Iterator и совершите итерации с ними при помощи цикла for.
 
 # Пример выполняемого кода:
 try:
@@ -76,19 +54,22 @@ except StepValueError:
     print('Шаг указан неверно')
 
 iter2 = Iterator(-5, 1)
-iter3 = Iterator(6, 15, 2)
-iter4 = Iterator(5, 1, -1)
-iter5 = Iterator(10, 1)
-
 for i in iter2:
     print(i, end=' ')
 print()
+
+iter3 = Iterator(6, 15, 2)
 for i in iter3:
     print(i, end=' ')
 print()
+
+
+iter4 = Iterator(5, 1, -1)
 for i in iter4:
     print(i, end=' ')
 print()
+
+iter5 = Iterator(10, 1)
 for i in iter5:
     print(i, end=' ')
 print()
