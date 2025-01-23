d0169f343735aa3bd0b435b6790eb86ddac8a693
proven_ak <proven@ya.ru>
2025-01-23 10:54:21 +0300

    mod_9_5 edit code

--- module_9/module_9_5.py
+++ module_9/module_9_5.py
@@ -45,6 +45,7 @@ def __next__(self):
         self.pointer += self.step
         return current
 
+
 class StepValueError(Exception):
     pass
 
@@ -64,30 +65,30 @@ class StepValueError(Exception):
 # Создайте несколько объектов класса Iterator и совершите итерации с ними при помощи цикла for.
 
 # Пример выполняемого кода:
-# try:
-# iter1 = Iterator(100, 200, 0)
-# for i in iter1:
-# print(i, end=' ')
-# except StepValueError:
-# print('Шаг указан неверно')
+try:
+    iter1 = Iterator(100, 200, 0)
+    for i in iter1:
+        print(i, end=' ')
+except StepValueError:
+    print('Шаг указан неверно')
 
-# iter2 = Iterator(-5, 1)
-# iter3 = Iterator(6, 15, 2)
-# iter4 = Iterator(5, 1, -1)
-# iter5 = Iterator(10, 1)
+iter2 = Iterator(-5, 1)
+iter3 = Iterator(6, 15, 2)
+iter4 = Iterator(5, 1, -1)
+iter5 = Iterator(10, 1)
 
-# for i in iter2:
-#     print(i, end=' ')
-#     print()
-# for i in iter3:
-#     print(i, end=' ')
-#     print()
-# for i in iter4:
-#     print(i, end=' ')
-#     print()
-# for i in iter5:
-#     print(i, end=' ')
-#     print()
+for i in iter2:
+    print(i, end=' ')
+print()
+for i in iter3:
+    print(i, end=' ')
+print()
+for i in iter4:
+    print(i, end=' ')
+print()
+for i in iter5:
+    print(i, end=' ')
+print()
 
 # Вывод на консоль:
 # Шаг указан неверно
