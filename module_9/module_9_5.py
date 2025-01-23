cf62da130ebd1767b47f06a6966c39a2f232a6de
proven_ak <proven@ya.ru>
2025-01-23 10:32:24 +0300

    mod_9_5 add def __next__

--- module_9/module_9_5.py
+++ module_9/module_9_5.py
@@ -30,8 +30,14 @@ def __init__(self, start, stop, step=1):
     if self.step == 0:
         raise StepValueError('шаг не может быть равен 0')
 
-def __iter__ ():
-# сброс значение pointer на start и возвращающий сам объект итератора.
+def __iter__ (self):
+    # сброс значение pointer на start и возвращающий сам объект итератора.
+
+def __next__(self):
+    pointer = 0
+    # увеличение атрибута pointer на step. В зависимости от знака атрибута step
+    if pointer > self.stop or pointer < self.stop:
+        return
 
 class StepValueError(Exception):
     pass
