44ced12fb3603b97eb19ee0c09f3e51d26847f24
proven_ak <proven@ya.ru>
2025-01-26 19:56:55 +0300

    mod_9_6 edit def all_variants

--- module_9/module_9_6.py
+++ module_9/module_9_6.py
@@ -19,17 +19,17 @@
 # Пример результата выполнения программы:
 
 
-# def all_variants(text):
-#     for _len in range(len(text)):
-#         for _start in range(len(text) + 1):
-#             yield text[_start:_len]
+def all_variants(text):
+    for _len in range(1, len(text) + 1):                # Длина подстроки от 1 до длины строки
+        for _start in range(len(text) - _len + 1):      # Все начальные индексы для данной длины
+            yield text[_start:_start + _len]            # Подстрока от _start длиной _len
 
 
 # Пример работы функции:
 
-# a = all_variants("abc")
-# for i in a:
-#     print(i)
+a = all_variants("abc")
+for i in a:
+    print(i)
 
 """
 Вывод на консоль:
@@ -40,7 +40,3 @@ ab
 bc
 abc
 """
-text = "abc"
-for _len in range(1, len(text) + 1):  # Длина подстроки от 1 до длины строки
-    for _start in range(len(text) - _len + 1):  # Все начальные индексы для данной длины
-        print(text[_start:_start + _len])  # Подстрока от _start длиной _len
